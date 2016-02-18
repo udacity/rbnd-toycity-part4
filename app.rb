b0b353d1852150453106c9918446bd25ccdde1ad
Julia Van Cleve <julia@jmv.me>
2016-02-18 10:58:04 -0800

    Change order of app.rb output

--- app.rb
+++ app.rb
@@ -12,20 +12,9 @@ include Analyzable
 # puts "RETURN ALL PRODUCTS"
 # puts "-------------------"
 # print Product.all
-#
-# puts "------------------------------"
-# puts "TEST ERROR HANDLING FOR DELETE"
-# puts "------------------------------"
-# print Product.destroy(1000000000000)
-#
-# puts "----------------------------"
-# puts "TEST ERROR HANDLING FOR FIND"
-# puts "----------------------------"
-# print Product.find(1000000000000)
-#
 # puts
 # puts "-----------------"
-# puts "CREATE A PRODUCT"
+# puts "CREATE PRODUCTS"
 # puts "-----------------"
 # print Product.create(brand: "Udacity", name: "yoyo", price: 10.00)
 # print Product.create(brand: "Bouncy", name: "ball", price: 12.00)
@@ -33,6 +22,21 @@ include Analyzable
 # print Product.create(brand: "MySoft", name: "stuffed animal", price: 2.09)
 # print Product.create(brand: "AllMy", name: "yoyo", price: 1.00)
 # print Product.create(brand: "Udacity", name: "doll", price: 5.99)
+#
+# puts "-------------------"
+# puts "RETURN ALL PRODUCTS"
+# puts "-------------------"
+# print Product.all
+#
+# puts "--------------------------------------------------------------"
+# puts "TEST ERROR HANDLING FOR DELETE - COMMENT OUT BEFORE SUBMITTING"
+# puts "--------------------------------------------------------------"
+# print Product.destroy(1000000000000)
+#
+# puts "------------------------------------------------------------"
+# puts "TEST ERROR HANDLING FOR FIND - COMMENT OUT BEFORE SUBMITTING"
+# puts "------------------------------------------------------------"
+# print Product.find(1000000000000)
 #
 # puts
 # puts "--------------------"
