fc978931b4db90c58692256906b0f6c2fc0daa0c
Chris Saden <chris.saden@udacity.com>
2016-05-03 16:05:57 -0700

    use class variable for csv file

--- lib/product.rb
+++ lib/product.rb
@@ -23,8 +23,7 @@ class Product < Udacidata
     # If it exists, increment and use this value
     # Otherwise, use 0 as starting ID number
     def get_last_id
-      file = File.dirname(__FILE__) + "/../data/data.csv"
-      last_id = File.exist?(file) ? CSV.read(file).last[0].to_i + 1 : nil
+      last_id = File.exist?(@@file) ? CSV.read(@@file).last[0].to_i + 1 : nil
       @@count_class_instances = last_id || 0
     end
 
