e212cb80865c67a68a472d3f564aeb97433f205e
Chris Saden <chris.saden@udacity.com>
2016-05-03 16:06:30 -0700

    implement create, all, first, and last class methods

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,37 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+
+  @@file = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create attributes=nil
+    new_data = self.new attributes # create the object
+    # If the object's data is already in the database
+    CSV.foreach(@@file, headers: true) do |row|
+      if row["id"] == new_data.id
+        return new_data # return the object
+      end
+    end
+    # If the object's data is not in the database
+    CSV.open(@@file, "a") do |csv| # save the data in the database
+      csv << [new_data.id, new_data.brand, new_data.name, new_data.price]
+    end
+    return new_data # return the object
+  end
+
+  def self.all
+    products = []
+    CSV.foreach(@@file, headers: true) do |row|
+      products << new(id: row["id"], brand: row["brand"], name: row["product"], price: row["price"])
+    end
+    products
+  end
+
+  def self.first n=1
+    n == 1 ? self.all.first : self.all.first(n)
+  end
+
+  def self.last n=1
+    n == 1 ? self.all.last : self.all.last(n)
+  end
 end
--- tests/udacidata_test.rb
+++ tests/udacidata_test.rb
@@ -21,79 +21,79 @@ class TestUdacidata < MiniTest::Test
     assert(after == before + 5)
   end
 
-  # def test_create_method_returns_product_object
-  #    product = Product.create(brand: "ColtToys", name: "Orchid Plant", price: 2.00)
-  #    assert_instance_of(Product, product)
-  # end
-  #
-  # def test_all_method_returns_array_data_type
-  #   array_of_products = Product.all
-  #   assert_kind_of(Array, array_of_products)
-  # end
-  #
-  # def test_all_method_returns_array_of_products
-  #   array_of_products = Product.all
-  #   array_of_products.each do |product|
-  #     assert_instance_of(Product, product)
-  #   end
-  # end
-  #
-  # def test_all_method_returns_all_products_in_database
-  #   expected = CSV.read(@data_path).drop(1).length
-  #   actual = Product.all.length
-  #   assert_equal(expected, actual)
-  # end
-  #
-  # def test_first_method_returns_product_object
-  #   product = Product.first
-  #   assert_instance_of(Product, product)
-  # end
-  #
-  # def test_first_method_returns_correct_product
-  #   product = Product.first
-  #   data = CSV.read(@data_path).drop(1)
-  #   assert_equal(data.first[0].to_i, product.id)
-  # end
-  #
-  # def test_first_n_returns_array
-  #   array_of_products = Product.first(3)
-  #   assert_kind_of(Array, array_of_products)
-  # end
-  #
-  # def test_first_n_returns_correct_products
-  #   array_of_products = Product.first(3)
-  #   data = CSV.read(@data_path).drop(1)
-  #   array_of_products.each_with_index do |product, index|
-  #     assert_equal(data[index][0].to_i, product.id)
-  #   end
-  # end
-  #
-  # def test_first_n_returns_correct_number_of_products
-  #   array_of_products = Product.first(3)
-  #   assert_equal(3, array_of_products.size)
-  # end
-  #
-  # def test_last_method_returns_product_object
-  #   product = Product.last
-  #   assert_instance_of(Product, product)
-  # end
-  #
-  # def test_last_method_returns_correct_product
-  #   product = Product.last
-  #   data = CSV.read(@data_path).drop(1)
-  #   assert_equal(data.last[0].to_i, product.id)
-  # end
-  #
-  # def test_last_n_returns_array_type
-  #   array_of_products = Product.last(3)
-  #   assert_kind_of(Array, array_of_products)
-  # end
-  #
-  # def test_last_n_returns_correct_number_of_products
-  #   array_of_products = Product.last(3)
-  #   assert_equal(3, array_of_products.size)
-  # end
-  #
+  def test_create_method_returns_product_object
+     product = Product.create(brand: "ColtToys", name: "Orchid Plant", price: 2.00)
+     assert_instance_of(Product, product)
+  end
+
+  def test_all_method_returns_array_data_type
+    array_of_products = Product.all
+    assert_kind_of(Array, array_of_products)
+  end
+
+  def test_all_method_returns_array_of_products
+    array_of_products = Product.all
+    array_of_products.each do |product|
+      assert_instance_of(Product, product)
+    end
+  end
+
+  def test_all_method_returns_all_products_in_database
+    expected = CSV.read(@data_path).drop(1).length
+    actual = Product.all.length
+    assert_equal(expected, actual)
+  end
+
+  def test_first_method_returns_product_object
+    product = Product.first
+    assert_instance_of(Product, product)
+  end
+
+  def test_first_method_returns_correct_product
+    product = Product.first
+    data = CSV.read(@data_path).drop(1)
+    assert_equal(data.first[0].to_i, product.id)
+  end
+
+  def test_first_n_returns_array
+    array_of_products = Product.first(3)
+    assert_kind_of(Array, array_of_products)
+  end
+
+  def test_first_n_returns_correct_products
+    array_of_products = Product.first(3)
+    data = CSV.read(@data_path).drop(1)
+    array_of_products.each_with_index do |product, index|
+      assert_equal(data[index][0].to_i, product.id)
+    end
+  end
+
+  def test_first_n_returns_correct_number_of_products
+    array_of_products = Product.first(3)
+    assert_equal(3, array_of_products.size)
+  end
+
+  def test_last_method_returns_product_object
+    product = Product.last
+    assert_instance_of(Product, product)
+  end
+
+  def test_last_method_returns_correct_product
+    product = Product.last
+    data = CSV.read(@data_path).drop(1)
+    assert_equal(data.last[0].to_i, product.id)
+  end
+
+  def test_last_n_returns_array_type
+    array_of_products = Product.last(3)
+    assert_kind_of(Array, array_of_products)
+  end
+
+  def test_last_n_returns_correct_number_of_products
+    array_of_products = Product.last(3)
+    assert_equal(3, array_of_products.size)
+  end
+
   # def test_find_method_returns_correct_product
   #   product = Product.find(5)
   #   assert_equal(5, product.id)
