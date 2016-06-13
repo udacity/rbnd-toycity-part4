48a3956dffe44e8531aeb1546563e26a85e0297c
Paul Haddad <paul.haddad@yello.co>
2016-06-13 12:33:55 -0500

    Print out all of app.rb

--- app.rb
+++ app.rb
@@ -74,59 +74,59 @@ puts "FIND PRODUCT 4 AND UPDATE BRAND"
 puts "-------------------------------"
 print Product.find(4).update(brand: "NewBrand")
 
-# puts
-# puts "--------------"
-# puts "FIND PRODUCT 1"
-# puts "--------------"
-# print Product.find(1)
-#
-# puts
-# puts "-----------------"
-# puts "DESTROY PRODUCT 3"
-# puts "-----------------"
-# print Product.destroy(3)
-#
-# puts
-# puts "-------------------------------"
-# puts "RETURN PRODUCTS WITH BRAND UDACITY"
-# puts "-------------------------------"
-# print Product.where(brand: "Udacity")
-#
-# puts
-# puts "-------------------------------------"
-# puts "RETURN FIRST PRODUCT WITH GIVEN BRAND"
-# puts "-------------------------------------"
-# print Product.find_by_brand("Udacity")
-# puts
-#
-# puts
-# puts "------------------------------------"
-# puts "RETURN FIRST PRODUCT WITH GIVEN NAME"
-# puts "------------------------------------"
-# print Product.find_by_name("yoyo")
-# puts
-#
-# puts
-# puts "----------------------------"
-# puts "RETURN HASH WITH NAME COUNTS"
-# puts "----------------------------"
-# print Analyzable::count_by_name(Product.all)
-#
-# puts
-# puts "----------------------------"
-# puts "RETURN HASH WITH BRAND COUNTS"
-# puts "----------------------------"
-# print Analyzable::count_by_brand(Product.all)
-#
-# puts
-# puts "--------------------"
-# puts "RETURN AVERAGE PRICE"
-# puts "--------------------"
-# print Analyzable::average_price(Product.all)
-#
-# puts
-# puts "--------------------"
-# puts "PRINT SUMMARY REPORT"
-# puts "--------------------"
-# print Analyzable::print_report(Product.all)
-# puts
+puts
+puts "--------------"
+puts "FIND PRODUCT 1"
+puts "--------------"
+print Product.find(1)
+
+puts
+puts "-----------------"
+puts "DESTROY PRODUCT 3"
+puts "-----------------"
+print Product.destroy(3)
+
+puts
+puts "-------------------------------"
+puts "RETURN PRODUCTS WITH BRAND UDACITY"
+puts "-------------------------------"
+print Product.where(brand: "Udacity")
+
+puts
+puts "-------------------------------------"
+puts "RETURN FIRST PRODUCT WITH GIVEN BRAND"
+puts "-------------------------------------"
+print Product.find_by_brand("Udacity")
+puts
+
+puts
+puts "------------------------------------"
+puts "RETURN FIRST PRODUCT WITH GIVEN NAME"
+puts "------------------------------------"
+print Product.find_by_name("yoyo")
+puts
+
+puts
+puts "----------------------------"
+puts "RETURN HASH WITH NAME COUNTS"
+puts "----------------------------"
+print Analyzable::count_by_name(Product.all)
+
+puts
+puts "----------------------------"
+puts "RETURN HASH WITH BRAND COUNTS"
+puts "----------------------------"
+print Analyzable::count_by_brand(Product.all)
+
+puts
+puts "--------------------"
+puts "RETURN AVERAGE PRICE"
+puts "--------------------"
+print Analyzable::average_price(Product.all)
+
+puts
+puts "--------------------"
+puts "PRINT SUMMARY REPORT"
+puts "--------------------"
+print Analyzable::print_report(Product.all)
+puts
