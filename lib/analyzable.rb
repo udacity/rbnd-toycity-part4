08e7e8dae4713a248e8b6bcf95b45320874fe840
Paul Haddad <paulh16@gmail.com>
2016-06-13 06:55:05 -0500

    Implement .average_price method

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,9 @@
 module Analyzable
-  # Your code goes here!
+  def self.average_price(items)
+    total_price = items.inject(0) do |total, item|
+      total += item.price.to_f
+    end
+
+    (total_price / items.count).round(2)
+  end
 end
