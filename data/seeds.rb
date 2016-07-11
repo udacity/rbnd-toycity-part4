ecb1e30d9026d0176cfd44816e559cd1f695fa1a
skamkar <sjk@zestfinance.com>
2016-07-10 20:43:54 -0700

    Create random brand, name, price with Faker

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,5 +4,10 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-  # Your code goes here!
+
+  100.times do
+    Product.create( brand: Faker::Company.name, 
+                    name: Faker::Commerce.product_name, 
+                    price: Faker::Commerece.price )
+  end
 end
