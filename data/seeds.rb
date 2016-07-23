0581ad8b4b42e150bdf4848a4ec8984fc49afb75
skamkar <sjk@zestfinance.com>
2016-07-23 14:21:24 -0700

    Correct typo for faker gem

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,10 +4,9 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-
   100.times do
     Product.create( brand: Faker::Company.name, 
                     name: Faker::Commerce.product_name, 
-                    price: Faker::Commerece.price )
+                    price: Faker::Commerce.price )
   end
 end
